8413fbc21bfbae9f18f1214d451cec977519d849
Chalkwal <Chalkwal.com>
2022-09-11 17:21:27 -0700

    Task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entrypoint
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	if (ch != 'e' && ch != 'q')
+	{
+		putchar(ch);
+	}
+	ch++;
+	putchar('\n');
+	return (0);
+}
