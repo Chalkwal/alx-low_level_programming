67cc52925fb0c95ce2e290a722ef03d27b69bf84
Chalkwal <Chalkwal.com>
2022-09-20 07:36:37 -0700

    Task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,13 @@
  * main - entry point
  * Return: Always 0 (success)
  */
-int main(void);
-{	_putchar('_');
+int main(void)
+{
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	-putchar('c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
